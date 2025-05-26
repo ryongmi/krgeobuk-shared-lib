7639e969e9ae411df407183288721fd2e828bd94
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-05-26 23:51:37 +0900

    feat(setting) : eslint, preter 등 기초 세팅

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # krgeobuk-shared-lib
+
 krgeobuk 관련 서비스에서 사용할 공통 패키지
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,29 @@
-import js from "@eslint/js";
+// import js from '@eslint/js';
 import globals from "globals";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: {
+      parser: tsParser,
+      globals: { ...globals.browser, ...globals.node },
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      // 원하는 룰을 여기 추가하세요.
+      // 예시:
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      "no-console": "off",
+    },
+    extends: ["plugin:@typescript-eslint/recommended", "eslint:recommended"],
+  },
 ]);
