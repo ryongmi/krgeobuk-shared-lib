ea9cc456a3615f430cdccb716dbb83524a16ccc5
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-05-28 00:25:03 +0900

    12312

--- packages/eslint/base.mjs
+++ packages/eslint/base.mjs
@@ -32,8 +32,16 @@
 import globals from "globals";
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import js from "@eslint/js";
+import typescriptEslint from "typescript-eslint";
 
 export default [
+  // ESLint 기본 추천 설정
+  js.configs.recommended,
+  
+  // TypeScript ESLint 추천 설정들
+  ...typescriptEslint.configs.recommended,
+  
   {
     ignores: ["dist", "node_modules"], // 기존 .eslintignore 대체
   },
