a37439a710253e41df851c37735c6870d1948bc3
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-06-15 01:12:57 +0900

    feat(swagger) - SwaggerApiPropertyOptional 데코레이터 추가

--- packages/swagger/src/decorators/api-property.decorator.ts
+++ packages/swagger/src/decorators/api-property.decorator.ts
@@ -1,11 +1,20 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { SwaggerApiPropertyOptions } from '../interface';
 
 /**
  *
- * @param tags Api 제목
+ * @param tags Api 제목 - 필수
  * @returns
  */
 export const SwaggerApiProperty = (param: SwaggerApiPropertyOptions): PropertyDecorator => {
-  return ApiProperty({ ...param, required: param.required ?? false });
+  return ApiProperty({ ...param, required: param.required ?? true });
+};
+
+/**
+ *
+ * @param tags Api 제목 - 옵션
+ * @returns
+ */
+export const SwaggerApiPropertyOptional = (param: SwaggerApiPropertyOptions): PropertyDecorator => {
+  return ApiPropertyOptional({ ...param, required: param.required ?? false });
 };
