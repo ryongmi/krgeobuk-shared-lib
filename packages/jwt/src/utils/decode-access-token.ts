011b1910e590c8f2143d0a789fc65e9ee9014592
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-06-17 13:08:35 +0900

    fix(jwt) - decodeAccessToken 에러 제어 삭제

--- packages/jwt/src/utils/decode-access-token.ts
+++ packages/jwt/src/utils/decode-access-token.ts
@@ -5,11 +5,5 @@ import type { DecodeAccessTokenOptions, JwtPayload } from '@krgeobuk/jwt/src/int
 export function decodeAccessToken(options: DecodeAccessTokenOptions): JwtPayload {
   const { token, secret } = options;
 
-  try {
-    return jwt.verify(token, secret) as JwtPayload;
-  } catch (error: unknown) {
-    throw new Error(
-      `Invalid Access Token: ${error instanceof Error ? error.message : 'Unknown error'}`
-    );
-  }
+  return jwt.verify(token, secret) as JwtPayload;
 }
