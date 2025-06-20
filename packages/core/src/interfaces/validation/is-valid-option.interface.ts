334e4f5e7e1f17871dcf15a7a8ada3ea283823e0
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-06-20 13:03:34 +0900

    feat(core) - array포함 interface option 추가

--- packages/core/src/interfaces/validation/is-valid-option.interface.ts
+++ packages/core/src/interfaces/validation/is-valid-option.interface.ts
@@ -4,6 +4,12 @@ export interface IsValidOptions {
   message?: string;
 }
 
+export interface IsValidAllowedOptions {
+  isOptional?: boolean;
+  isExpose?: boolean;
+  allowed?: string[];
+}
+
 type ClassConstructor<T> = abstract new (...args: unknown[]) => T;
 
 export interface IsValidNestedOptions<T> {
