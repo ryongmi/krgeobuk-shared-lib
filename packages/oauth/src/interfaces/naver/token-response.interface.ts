d37261ae606af12881a52dbf1a63ae22621080a7
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-06-21 23:05:07 +0900

    feat(oauth) - naver token 응답 인터페이스 추가

---
+++ packages/oauth/src/interfaces/naver/token-response.interface.ts
@@ -0,0 +1,15 @@
+import type { NaverTokenType } from '@krgeobuk/oauth/src/enum';
+
+export interface NaverTokenResponse {
+  accessToken: string;
+  refreshToken: string;
+  tokenType: NaverTokenType;
+  expiresIn: number;
+}
+
+// {
+//     "access_token":"AAAAQosjWDJieBiQZc3to9YQp6HDLvrmyKC+6+iZ3gq7qrkqf50ljZC+Lgoqrg",
+//     "refresh_token":"c8ceMEJisO4Se7uGisHoX0f5JEii7JnipglQipkOn5Zp3tyP7dHQoP0zNKHUq2gY",
+//     "token_type":"bearer",
+//     "expires_in":"3600"
+// }
