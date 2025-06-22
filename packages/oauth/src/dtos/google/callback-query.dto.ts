bdc27ee57be16b9e2470e806a4a07c40263430ac
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-06-22 20:24:49 +0900

    feat(oauth) - google callback query dto 및 interface에 state 속성 추가

--- packages/oauth/src/dtos/google/callback-query.dto.ts
+++ packages/oauth/src/dtos/google/callback-query.dto.ts
@@ -1,7 +1,10 @@
-import { IsValidGoogleCode } from '@krgeobuk/oauth/src/decorators';
+import { IsValidGoogleCode, IsValidGoogleState } from '@krgeobuk/oauth/src/decorators';
 import type { GoogleOAuthCallbackQuery } from '@krgeobuk/oauth/src/interfaces';
 
 export class GoogleOAuthCallbackQueryDto implements GoogleOAuthCallbackQuery {
   @IsValidGoogleCode()
   code!: string;
+
+  @IsValidGoogleState()
+  state!: string;
 }
--- packages/oauth/src/interfaces/google/callback-query.interface.ts
+++ packages/oauth/src/interfaces/google/callback-query.interface.ts
@@ -1,3 +1,4 @@
 export interface GoogleOAuthCallbackQuery {
   code: string;
+  state: string;
 }
