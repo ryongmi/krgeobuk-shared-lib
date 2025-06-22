52c89051c4d5f9bb7fdfc7d90d50a9b8bb827d86
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-06-22 20:30:36 +0900

    feat(oauth) - naver user profile 응답 interface 추가

---
+++ packages/oauth/src/interfaces/naver/user-profile-response.interface.ts
@@ -0,0 +1,27 @@
+import type { NaverGenderTypeValue } from '@krgeobuk/oauth/src/types';
+
+export interface NaverUserProfileResponse {
+  id: string;
+  email: string;
+  nickname: string;
+  profileImage: string;
+  age: string;
+  gender: NaverGenderTypeValue;
+  name: string;
+  birthday: string;
+  birthyear: string;
+  mobile: string;
+}
+
+// {
+//     "id": "32742776",
+//     "email": "openapi@naver.com",
+//     "nickname": "OpenAPI",
+//     "profile_image": "https://ssl.pstatic.net/static/pwe/address/nodata_33x33.gif",
+//     "age": "40-49",
+//     "gender": "F",
+//     "name": "오픈 API",
+//     "birthday": "10-01",
+//     "birthyear": "1900",
+//     "mobile": "010-0000-0000"
+//  }
