2a1fa03365886082497298212ed72239b58f488b
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-06-23 15:28:46 +0900

    build(core) - exporess 경로만 따로 분리해서 작성

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -6,6 +6,7 @@ export * from './enum/index.js';
 export * from './filters/index.js';
 export * from './interceptors/index.js';
 export * from './interfaces/index.js';
+export * from './interfaces/express/index.js'; // express request type용 이것만 따로 분리함
 export * from './repositories/index.js';
 export * from './utils/index.js';
 // export * from "./types";
