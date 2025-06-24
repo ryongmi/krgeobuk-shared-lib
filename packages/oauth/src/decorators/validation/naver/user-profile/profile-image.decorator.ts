b7578d9fcf132f5c003a6bf01872efc67c695526
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-06-24 20:32:06 +0900

    feat - naver porfile image validation expose 추가

--- packages/oauth/src/decorators/validation/naver/user-profile/profile-image.decorator.ts
+++ packages/oauth/src/decorators/validation/naver/user-profile/profile-image.decorator.ts
@@ -21,9 +21,8 @@ export function IsValidNaverProfileImage(options: IsValidOptions = {}): Property
       { message: 'Profile image must be a valid HTTPS URL' }
     ),
     MaxLength(2048, { message: 'Profile image URL is too long' }), // URL 길이 제한,
-    Expose({ name: 'profile_image' }),
   ];
-  const exposeDators = [Expose()];
+  const exposeDators = [Expose({ name: 'profile_image' })];
   const optionality = isOptional
     ? IsOptional()
     : IsNotEmpty({ message: 'Naver OAuth User ProfileImage URL은 필수입니다' });
