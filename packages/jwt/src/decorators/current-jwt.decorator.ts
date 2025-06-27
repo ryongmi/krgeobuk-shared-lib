8d70ee177d730b465fdd7db76eba7be54472fcac
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-06-27 12:41:20 +0900

    feat(jwt) - current jwt decorator 추가

---
+++ packages/jwt/src/decorators/current-jwt.decorator.ts
@@ -0,0 +1,9 @@
+import '@krgeobuk/core/interfaces/express';
+
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import { Request } from 'express';
+
+export const CurrentJwt = createParamDecorator((_: unknown, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest<Request>();
+  return request.jwt;
+});
--- packages/jwt/src/decorators/index.ts
+++ packages/jwt/src/decorators/index.ts
@@ -1,1 +1,2 @@
 export * from './validation/index.js';
+export * from './current-jwt.decorator.js';
