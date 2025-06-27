2ef97ed86f17a7dad8612198eb018b8c2d3806a5
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-06-27 12:55:17 +0900

    fix(user) - profileImage decorator 파일 명칭 변경

--- packages/user/src/decorators/validation/index.ts
+++ packages/user/src/decorators/validation/index.ts
@@ -3,5 +3,5 @@ export * from './is-email-verified.decorator.js';
 export * from './is-integrated.decorator.js';
 export * from './nickname.decorator.js';
 export * from './password.decorator.js';
-export * from './profile-image.decorator.js';
+export * from './profile-image-url.decorator.js';
 export * from './username.decorator.js';
