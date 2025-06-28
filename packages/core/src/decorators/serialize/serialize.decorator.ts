d08a25268f25a2f7cfd706f4d8ff9d82516327ad
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-06-28 21:50:47 +0900

    fix(core) - 전역 인터셉터 사용 예정으로 기존 serialize decorator 인터셉터 삭제

--- packages/core/src/decorators/serialize/serialize.decorator.ts
+++ packages/core/src/decorators/serialize/serialize.decorator.ts
@@ -1,20 +1,8 @@
-import { SetMetadata, UseInterceptors } from '@nestjs/common';
+import { SetMetadata } from '@nestjs/common';
 
 import { SERIALIZE_META_KEY } from '../../constants/index.js';
-import { SerializerInterceptor } from '../../interceptors/index.js';
 import { SerializeOptions } from '../../interfaces/index.js';
 
 export function Serialize(options: SerializeOptions): MethodDecorator {
-  // return function (target: object, key: string | symbol, descriptor: PropertyDescriptor) {
-  //   SetMetadata(SERIALIZE_META_KEY, options)(target, key, descriptor);
-  //   UseInterceptors(SerializerInterceptor)(target, key, descriptor);
-  // };
-
-  const setMeta = SetMetadata(SERIALIZE_META_KEY, options);
-  const useInterceptor = UseInterceptors(SerializerInterceptor);
-
-  return function (target: object, key: string | symbol, descriptor: PropertyDescriptor) {
-    setMeta(target, key, descriptor);
-    useInterceptor(target, key, descriptor);
-  };
+  return SetMetadata(SERIALIZE_META_KEY, options);
 }
