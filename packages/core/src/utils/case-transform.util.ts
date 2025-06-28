4a8facc605ca08294caffe35fc5317c3b4f2014f
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-06-29 01:31:35 +0900

    fix(core) - toSnakeCase 함수 제외 타입 추가
    - Date 타입이 변환되는지 값이 {} 로 나와서 제외함
    - 추가적으로 다른 타입도 그럴 가능성이 있어 몇 가지 제외함

--- packages/core/src/utils/case-transform.util.ts
+++ packages/core/src/utils/case-transform.util.ts
@@ -3,11 +3,25 @@ import { snakeCase } from 'lodash-es';
 export function toSnakeCase(obj: unknown): unknown {
   if (Array.isArray(obj)) {
     return obj.map(toSnakeCase);
-  } else if (obj !== null && typeof obj === 'object') {
-    return Object.fromEntries(
-      Object.entries(obj).map(([key, value]) => [snakeCase(key), toSnakeCase(value)])
-    );
-  } else {
+  }
+
+  if (obj === null || typeof obj !== 'object') {
     return obj;
   }
+
+  // 순회 제외할 타입들
+  if (
+    obj instanceof Date ||
+    obj instanceof RegExp ||
+    obj instanceof Set ||
+    obj instanceof Map ||
+    typeof obj === 'bigint' ||
+    typeof obj === 'symbol'
+  ) {
+    return obj;
+  }
+
+  return Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => [snakeCase(key), toSnakeCase(value)])
+  );
 }
