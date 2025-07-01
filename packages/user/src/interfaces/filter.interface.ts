d4e99b7aab2d3b6a5894e9dc78e6a588536aa00a
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-07-01 15:34:33 +0900

    feat(user) - find용 filter interface 추가

---
+++ packages/user/src/interfaces/filter.interface.ts
@@ -0,0 +1,8 @@
+export interface UserFilter {
+  email?: string;
+  name?: string;
+  nickname?: string;
+  profileImageUrl?: string;
+  isEmailVerified?: boolean;
+  isIntegrated?: boolean;
+}
--- packages/user/src/interfaces/index.ts
+++ packages/user/src/interfaces/index.ts
@@ -1,5 +1,6 @@
 export * from './change-password.interface.js';
 export * from './detail.interface.js';
+export * from './filter.interface.js';
 export * from './logged-in-user.interface.js';
 export * from './search-query.interface.js';
 export * from './search-result.interface.js';
