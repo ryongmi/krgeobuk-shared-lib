ee5f97e64f7337c8402fefd9ffb74679ffcdd037
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-07-03 23:52:41 +0900

    chore(jwt) - 참조 순서 정리

--- packages/jwt/src/decorators/validation/access-token.decorator.ts
+++ packages/jwt/src/decorators/validation/access-token.decorator.ts
@@ -1,8 +1,9 @@
 import { applyDecorators } from '@nestjs/common';
-import { SwaggerApiProperty, SwaggerApiPropertyOptional } from '@krgeobuk/swagger/decorators';
-import { type IsValidOptions } from '@krgeobuk/core/interfaces';
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { Expose } from 'class-transformer';
+
+import { SwaggerApiProperty, SwaggerApiPropertyOptional } from '@krgeobuk/swagger/decorators';
+import { type IsValidOptions } from '@krgeobuk/core/interfaces';
 
 // accessToken 유효성 검사
 export function IsValidAccessToken(options: IsValidOptions = {}): PropertyDecorator {
