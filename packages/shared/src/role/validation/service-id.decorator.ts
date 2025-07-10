9b3294dbfc83f599fef03d6a3d0d51659a85a25c
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-07-10 14:34:18 +0900

    feat(shared) - role serviceId validation 추가

---
+++ packages/shared/src/role/validation/service-id.decorator.ts
@@ -0,0 +1,37 @@
+import { applyDecorators } from '@nestjs/common';
+
+import { IsUUID, IsOptional, IsNotEmpty } from 'class-validator';
+import { Expose } from 'class-transformer';
+
+import { SwaggerApiProperty, SwaggerApiPropertyOptional } from '@krgeobuk/swagger/decorators';
+import type { IsValidOptions } from '@krgeobuk/core/interfaces';
+
+export function IsValidServiceId(options: IsValidOptions = {}): PropertyDecorator {
+  const { isOptional = false } = options;
+
+  const propertyData = {
+    example: '123e4567-e89b-12d3-a456-426614174000',
+    description: '서비스 ID',
+    type: String,
+    format: 'uuid',
+  };
+  const apiDecorator = isOptional
+    ? SwaggerApiPropertyOptional(propertyData)
+    : SwaggerApiProperty(propertyData);
+  const validators = [IsUUID('4', { message: '서비스 ID는 유효한 UUID여야 합니다.' })];
+  const exposeDators = [Expose({ name: 'service_id' })];
+  const optionality = isOptional ? IsOptional() : IsNotEmpty({ message: '서비스 ID는 필수입니다' });
+
+  return applyDecorators(apiDecorator, optionality, ...validators, ...exposeDators);
+}
+
+export function ExposeServiceId(): PropertyDecorator {
+  const propertyData = {
+    example: '123e4567-e89b-12d3-a456-426614174000',
+    description: '서비스 ID',
+    type: String,
+    format: 'uuid',
+  };
+
+  return applyDecorators(SwaggerApiProperty(propertyData), Expose());
+}
