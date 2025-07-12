ee6d559439d9916fc17a42ab65bcdbf77792e97a
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-07-12 19:50:55 +0900

    feat(user) - TCP 관련 내용 추가

---
+++ packages/user/src/tcp/interfaces/params.interface.ts
@@ -0,0 +1,9 @@
+/**
+ * User 도메인 TCP 파라미터 인터페이스
+ * 사용자 관련 TCP 통신에서 사용되는 파라미터
+ */
+
+import type { UserIdParams } from '@krgeobuk/shared/user/interfaces';
+
+// shared의 기본 ID params 재사용
+export type TcpUserParams = UserIdParams;
