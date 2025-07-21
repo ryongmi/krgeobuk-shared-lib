9b56e03bb40eda9a59da2a02dcf4e2691777866c
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-07-21 22:42:52 +0900

    feat - nestjs jest 설정 추가

--- packages/jest/nest.mjs
+++ packages/jest/nest.mjs
@@ -1,17 +1,34 @@
-import base from './base.mjs';
-
 export default {
-  ...base,
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: 'tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^@modules/(.*)\\.js$': '<rootDir>/modules/$1',
+    '^@modules/(.*)$': '<rootDir>/modules/$1',
+    '^@common/(.*)\\.js$': '<rootDir>/common/$1',
+    '^@common/(.*)$': '<rootDir>/common/$1',
+    '^@config/(.*)\\.js$': '<rootDir>/config/$1',
+    '^@config/(.*)$': '<rootDir>/config/$1',
+    '^@database/(.*)\\.js$': '<rootDir>/database/$1',
+    '^@database/(.*)$': '<rootDir>/database/$1',
+    '^@krgeobuk/([^/]+)/(.*)$': '<rootDir>/../node_modules/@krgeobuk/$1/dist/$2',
+    '^@krgeobuk/([^/]+)$': '<rootDir>/../node_modules/@krgeobuk/$1/dist',
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transformIgnorePatterns: ['/node_modules/(?!(@krgeobuk|lodash-es|@nestjs)/).+\\.js$'],
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
   testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
 };
 
-// const base = require("./base.cjs");
-
-// module.exports = {
-//   ...base,
-//   preset: "ts-jest",
-//   testEnvironment: "node",
-//   moduleFileExtensions: ["ts", "js", "json", "node"],
-// };
