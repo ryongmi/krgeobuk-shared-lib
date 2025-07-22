7b19c7ccad7faef4e158e6004521532265145fd4
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-07-23 00:17:37 +0900

    feat(user) - profile 경로 index 추가

--- packages/user/src/dtos/index.ts
+++ packages/user/src/dtos/index.ts
@@ -1,6 +1,8 @@
 export * from './authorization.dto.js';
 export * from './change-password.dto.js';
 export * from './detail.dto.js';
+export * from './profile.dto.js';
 export * from './search-query.dto.js';
 export * from './search-result.dto.js';
 export * from './update-profile.dto.js';
+
