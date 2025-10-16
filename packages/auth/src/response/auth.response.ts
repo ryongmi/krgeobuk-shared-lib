bc676064aa60be7a5338dc0464cdb757b21dcf31
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-10-16 20:42:05 +0900

    fix - 로그인, 회원가입 status code 수정

--- packages/auth/src/response/auth.response.ts
+++ packages/auth/src/response/auth.response.ts
@@ -17,6 +17,18 @@ export class AuthResponse {
     statusCode: 204,
   };
 
+  static readonly SSO_LOGIN_REDIRECT = {
+    code: AuthCode.SSO_LOGIN_REDIRECT,
+    message: AuthMessage.SSO_LOGIN_REDIRECT,
+    statusCode: 200,
+  };
+
+  static readonly SSO_SIGNUP_REDIRECT = {
+    code: AuthCode.SSO_SIGNUP_REDIRECT,
+    message: AuthMessage.SSO_SIGNUP_REDIRECT,
+    statusCode: 200,
+  };
+
   static readonly REFRESH_SUCCESS = {
     code: AuthCode.REFRESH_SUCCESS,
     message: AuthMessage.REFRESH_SUCCESS,
@@ -30,18 +42,6 @@ export class AuthResponse {
    *   =============================================================================
    */
   /** */
-
-  static readonly SSO_LOGIN_REDIRECT = {
-    code: AuthCode.SSO_LOGIN_REDIRECT,
-    message: AuthMessage.SSO_LOGIN_REDIRECT,
-    statusCode: 302,
-  };
-
-  static readonly SSO_SIGNUP_REDIRECT = {
-    code: AuthCode.SSO_SIGNUP_REDIRECT,
-    message: AuthMessage.SSO_SIGNUP_REDIRECT,
-    statusCode: 302,
-  };
 
   static readonly SSO_LOGIN_START_REDIRECT = {
     code: AuthCode.SSO_LOGIN_START_REDIRECT,
